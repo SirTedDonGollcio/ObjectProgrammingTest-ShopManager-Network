02b13c40c86264db0ead0606caa9efa4a12b213b
Tadeusz Paweł Golczyk <t.golczyk@stud.elka.pw.edu.pl>
2019-05-07 23:01:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Witaj w aplikacji do zarzadzania siecia sklepow!\nTa czesc poswiecona jest na osobne placowki\nAutorem jest: Tadeusz Golczyk\nAplikacja zostala wykonana w ramach zajec z Projektowania Obiektowego\nOpiekun autora: M.W.Zodkiewicz\n";
+	cout << "Witaj w aplikacji do zarzadzania siecia sklepow!\nTa czesc poswiecona jest na osobne placowki\nAutorem jest: Tadeusz Golczyk\nAplikacja zostala wykonana w ramach zajec z Projektowania Obiektowego\nOpiekun autora: M.W.Zotkiewicz\n";
 	cout << "Co chcialbys przetestowac?\n";
 	
 	uzyjMenu();
