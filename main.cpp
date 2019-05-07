5cd46d5bcaee9cb5623eeabbcc82c7e60ef1ac4c
Tadeusz Paweł Golczyk <t.golczyk@stud.elka.pw.edu.pl>
2019-05-07 22:58:33 +0200

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "mainly.h"
+using namespace std;
+
+int main()
+{
+	cout << "Witaj w aplikacji do zarzadzania siecia sklepow!\nTa czesc poswiecona jest na osobne placowki\nAutorem jest: Tadeusz Golczyk\nAplikacja zostala wykonana w ramach zajec z Projektowania Obiektowego\nOpiekun autora: M.W.Zodkiewicz\n";
+	cout << "Co chcialbys przetestowac?\n";
+	
+	uzyjMenu();
+	return 0;
+}
